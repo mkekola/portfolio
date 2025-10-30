472b5e2f0a2f4b11ee406639d1ef469e1a28c390
Maria Kekola <maria.kekola@helsinki.fi>
2025-10-31 00:42:59 +0200

    Removed top header to move nav below hero.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,29 +1,9 @@
 <template>
-  <q-layout view="hHh lpR fFf">
-    <q-header class="bg-primary text-white" elevated>
-      <q-toolbar>
-        <q-toolbar-title class="text-weight-bold">MK</q-toolbar-title>
-        <q-btn flat dense no-caps @click="to('#about')" label="About" />
-        <q-btn flat dense no-caps @click="to('#education')" label="Education" />
-        <q-btn flat dense no-caps @click="to('#experience')" label="Experience" />
-        <q-btn flat dense no-caps @click="to('#skills')" label="Skills" />
-        <q-btn flat dense no-caps @click="to('#contact')" label="Contact" />
-        <q-space />
-      </q-toolbar>
-    </q-header>
-
-    <q-page-container><router-view /></q-page-container>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
   </q-layout>
 </template>
 
-<script setup lang="ts">
-function to(sel: string) {
-  document.querySelector(sel)?.scrollIntoView({ behavior: 'smooth', block: 'start' });
-}
-</script>
-
-<style>
-html {
-  scroll-behavior: smooth;
-}
-</style>
+<script setup lang="ts"></script>
